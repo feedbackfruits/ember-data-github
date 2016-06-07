b1d56a0faf201bf73be950906e94e75a542e69c5
Steffan Sluis <steffansluis@gmail.com>
2016-06-07 17:43:31 +0200

    Add labels

--- addon/models/github-issue.js
+++ addon/models/github-issue.js
@@ -1,9 +1,12 @@
 import DS from 'ember-data';
+import { hasMany } from 'ember-data/relationships';
 
 export default DS.Model.extend({
   title: DS.attr('string'),
   body: DS.attr('string'),
   number: DS.attr('number'),
   state: DS.attr('string'),
-  milestone: DS.attr('number')
+  milestone: DS.attr('number'),
+
+  labels: hasMany('githubLabel')
 });
---
+++ app/adapters/github-label.js
@@ -0,0 +1,3 @@
+import githubLabel from 'ember-data-github/adapters/github-label';
+
+export default githubLabel;
