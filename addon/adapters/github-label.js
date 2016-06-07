ec759c7c68b59fe82598b03d3cd6d8ec1ff0de67
Steffan Sluis <steffansluis@gmail.com>
2016-06-07 17:58:47 +0200

    JSHint fixes

--- addon/adapters/github-label.js
+++ addon/adapters/github-label.js
@@ -1,7 +1,7 @@
 import GithubRepositoryAdapter from './github-repository';
 
 export default GithubRepositoryAdapter.extend({
-  buildURL(type, id, snapshot, requestType, query) {
+  buildURL(type, id, snapshot) {
     let builtURL = this._super(type, id, snapshot);
 
     builtURL = builtURL.replace('labels', 'repos');
